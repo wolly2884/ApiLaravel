913d72f0a77319a8a86371329ded456a8cc19332
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2024-06-12 13:58:41 -0300

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+
 echo "Running composer"
-cp /etc/secrets/.env .env
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Clearing caches..."
 php artisan optimize:clear
+
+echo "generating application key..."
+php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
